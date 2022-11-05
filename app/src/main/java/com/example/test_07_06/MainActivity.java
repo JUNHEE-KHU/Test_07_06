e178aa6240088627bff0d6853144a86898ce64b6
junhee <pjh1313@khu.ac.kr>
2022-11-05 16:33:17 +0900

    Initial commit

---
+++ app/src/main/java/com/example/test_07_06/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.test_07_06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void mOnClick(View v) {
+        Resources res = getResources();
+        switch (v.getId()) {
+            case R.id.btnGetBool:
+                boolean bool_value = res.getBoolean(R.bool.test_bool);
+                Toast.makeText(this, "부울값 = " + bool_value, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnGetInt:
+                int int_value = res.getInteger(R.integer.test_int);
+                Toast.makeText(this, "정숫값 = " + int_value, Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
